0393052c738e57d0cbc5491a138f4ed9ef79d3bf
8mileverse <Ayodelu4@gmail.com>
2025-04-11 11:59:50 +0100

    build

--- Server/server.js
+++ Server/server.js
@@ -8,10 +8,11 @@ const app = express();
 const port = process.env.PORT || 5000;
 const frontendUrl = process.env.FRONTEND_URL || 'http://localhost:5173';  // Default to localhost if not set
 // const reactAppUrl = process.env.REACT_APP_API_URL || 'http://localhost:5173'; // Default to localhost if not set
-app.use(bodyParser.json());
+app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 corsOptions = {
-  origin: 'https://react-x-node.vercel.app/', // Replace with your frontend URL
+  origin: frontendUrl, // Replace with your frontend URL
   methods: ['GET', 'POST'], 
 };
 
