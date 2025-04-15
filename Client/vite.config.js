eb7e411e57a85b5ebf9d3a61e2171ffb41940932
8mileverse <Ayodelu4@gmail.com>
2025-04-15 11:29:34 +0100

    proxy:added

--- Client/vite.config.js
+++ Client/vite.config.js
@@ -7,4 +7,10 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   base: "/",
   plugins: [react(), tailwindcss()],
+
+  server: {
+    proxy: {
+      '/api': 'http://localhost:4100'
+    }
+  }
 })
